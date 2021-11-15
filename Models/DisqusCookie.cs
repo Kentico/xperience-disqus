3ca13203dde13278ff7e1536c3c87c27e3b0352f
Eric Dugre <ericd@kentico.com>
2021-11-15 12:17:55 -0500

    remove previous implementation

--- Models/DisqusCookie.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kentico.Xperience.Disqus.Models
-{
-    public class DisqusCookie
-    {
-        public string Access_Token { get; set; }
-
-        public string User_Id { get; set; }
-    }
-}
